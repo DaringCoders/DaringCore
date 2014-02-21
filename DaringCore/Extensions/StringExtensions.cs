c746ac2e6fc29f09c75330beedea8413e04ed3ac
Siva Katir <siva@daringcoders.com>
2014-02-20 20:06:00 -0800

    Init

---
+++ DaringCore/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace DaringCore.Extensions
+{
+    public static class StringExtensions
+    {
+        static public string EncodeTo64(this string value)
+        {
+            return Convert.ToBase64String(Encoding.ASCII.GetBytes(value));
+        }
+
+        static public string DecodeFrom64(this string value)
+        {
+            return Encoding.ASCII.GetString(Convert.FromBase64String(value)); ;
+        }
+    }
+}
