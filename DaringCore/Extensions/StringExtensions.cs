e0e1bb4780ae8e7071c718e2afeb51be7910c71d
Siva Katir <siva@daringcoders.com>
2014-02-20 20:11:18 -0800

    better names

--- DaringCore/Extensions/StringExtensions.cs
+++ DaringCore/Extensions/StringExtensions.cs
@@ -5,12 +5,12 @@ namespace DaringCore.Extensions
 {
     public static class StringExtensions
     {
-        static public string EncodeTo64(this string value)
+        static public string Base64Encode(this string value)
         {
             return Convert.ToBase64String(Encoding.ASCII.GetBytes(value));
         }
 
-        static public string DecodeFrom64(this string value)
+        static public string Base64Decode(this string value)
         {
             return Encoding.ASCII.GetString(Convert.FromBase64String(value)); ;
         }
