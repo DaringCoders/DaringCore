dc7a5f75575a8cc77ca341767d01a8f8bf221056
Siva Katir <siva@daringcoders.com>
2014-02-22 21:27:13 -0800

    Add AlphaNumericOnly String Method

--- DaringCore/Extensions/StringExtensions.cs
+++ DaringCore/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace DaringCore.Extensions
@@ -13,6 +14,11 @@ namespace DaringCore.Extensions
         static public string Base64Decode(this string value)
         {
             return Encoding.ASCII.GetString(Convert.FromBase64String(value)); ;
+        }
+
+        static public string AlphaNumericOnly(this string value)
+        {
+            return String.Concat(value.Where(Char.IsLetterOrDigit));
         }
     }
 }
